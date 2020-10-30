e3331b67a33610667ea825a137f759a1d808962f
nug <36500866+kouzato@users.noreply.github.com>
2020-10-30 13:50:04 +0700

    Add files via upload

---
+++ E41191450_MuhammadRahadianNugraha/minggu_ke_6/belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+//tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+//tipe data string (teks)
+$nama_lengkap = "Muhammad Rahadian Nugraha";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 50.2;
+
+//tipe data float
+$tinggi = 175.5;
+
+//tipe data boolean
+$menikah = false;
+
+echo "Nama : $nama_lengkap<br>";
+echo "Jenis Kelamin : $jenis_kelamin<br>";
+echo "Umur : $umur<br>";
+echo "Berat Badan : $berat<br>";
+echo "Tinggi Badan : $tinggi<br>";
+echo "Status Menikah : $menikah";
+?>
