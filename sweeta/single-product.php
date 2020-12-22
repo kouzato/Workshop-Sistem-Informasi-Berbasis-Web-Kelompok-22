feb22b43534e274b94f2781198fe1424bf473f6b
wildan1224 <72352880+wildan1224@users.noreply.github.com>
2020-12-22 23:15:20 +0700

    Add files via upload

---
+++ sweeta/single-product.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Sweeta - View Produk</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="assets/images/favicon.ico" type="img/x-icon" rel="shortcut icon">
+    <!-- All css files are included here. -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/helper.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Modernizr JS -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+
+    <div id="main-wrapper">
+
+        <?php include "header_web.php"?>
+
+
+
+       
+        <!-- Single Product Section Start -->
+        <div class="single-product-section section pt-60 pt-lg-40 pt-md-30 pt-sm-20 pt-xs-25 pb-100 pb-lg-80 pb-md-70 pb-sm-30 pb-xs-20" >
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="shop-area">
+                            <div class="row">
+                                <div class="col-md-6 pr-35 pr-lg-15 pr-md-15 pr-sm-15 pr-xs-15">
+                                    <!-- Product Details Left -->
+                                    <div class="product-details-left">
+                                        <div class="product-details-images">
+                                            <div class="lg-image">
+                                                <img src="./assets/images/product/large-product/l-product-1.jpg" alt="">
+                                                <a href="./assets/images/product/large-product/l-product-1.jpg" class="popup-img venobox" data-gall="myGallery"><i class="fa fa-expand"></i></a>
+                                            </div>
+                                            <div class="lg-image">
+                                                <img src="./assets/images/product/large-product/l-product-2.jpg" alt="">
+                                                <a href="./assets/images/product/large-product/l-product-2.jpg" class="popup-img venobox" data-gall="myGallery"><i class="fa fa-expand"></i></a>
+                                            </div>
+                                            <div class="lg-image">
+                                                <img src="./assets/images/product/large-product/l-product-3.jpg" alt="">
+                                                <a href="./assets/images/product/large-product/l-product-3.jpg" class="popup-img venobox" data-gall="myGallery"><i class="fa fa-expand"></i></a>
+                                            </div>
+                                            <div class="lg-image">
+                                                <img src="./assets/images/product/large-product/l-product-4.jpg" alt="">
+                                                <a href="./assets/images/product/large-product/l-product-4.jpg" class="popup-img venobox" data-gall="myGallery"><i class="fa fa-expand"></i></a>
+                                            </div>
+                                            <div class="lg-image">
+                                                <img src="./assets/images/product/large-product/l-product-5.jpg" alt="">
+                                                <a href="./assets/images/product/large-product/l-product-5.jpg" class="popup-img venobox" data-gall="myGallery"><i class="fa fa-expand"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="product-details-thumbs">
+                                            <div class="sm-image"><img src="./assets/images/product/small-product/s-product-1.jpg" alt="product image thumb"></div>
+                                            <div class="sm-image"><img src="./assets/images/product/small-product/s-product-2.jpg" alt="product image thumb"></div>
+                                            <div class="sm-image"><img src="./assets/images/product/small-product/s-product-3.jpg" alt="product image thumb"></div>
+                                            <div class="sm-image"><img src="./assets/images/product/small-product/s-product-4.jpg" alt="product image thumb"></div>
+                                            <div class="sm-image"><img src="./assets/images/product/small-product/s-product-5.jpg" alt="product image thumb"></div>
+                                        </div>
+                                    </div>
+                                    <!--Product Details Left -->
+                                </div>
+                                <div class="col-md-6">
+                                    <!--Product Details Content Start-->
+                                    <div class="product-details-content">
+                                        <!--Product Nav Start-->
+                                        <div class="product-nav">
+                                            <a href="#"><i class="fa fa-angle-left"></i></a>
+                                            <a href="#"><i class="fa fa-angle-right"></i></a>
+                                        </div>
+                                        <!--Product Nav End-->
+                                        <h2>Baju Tari A</h2>
+                                        
+                                        <div class="single-product-price">
+                                            <span class="price new-price">Rp. xxx.xxx</span>
+                                            <span class="regular-price">Rp. xxx.xxx</span>
+                                        </div>
+                                        <div class="product-description">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+                                        </div>
+                                        
+                                        <div class="single-product-quantity">
+                                            <form class="add-quantity" action="#">
+                                                <div class="product-quantity">
+                                                    <input value="1" type="number">
+                                                </div>
+                                                <div class="add-to-link">
+                                                    <button class="btn"><i class="fa fa-shopping-bag"></i>tambah</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        
+                                        <div class="product-meta">
+                                            <span class="posted-in">
+                                                Kategori : 
+                                                <a href="#">Kategori A</a>
+                                               
+                                            </span>
+                                        </div>
+                                        
+                                    </div>
+                                    <!--Product Details Content End-->
+                                </div >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Single Product Section End -->
+
+       
+        <!--Footer section start-->
+        <?php include "footer.php"?>
+
+       
+    </div>
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!-- All jquery file included here -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.22&key=AIzaSyDAq7MrCR1A2qIShmjbtLHSKjcEIEBEEwM"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
