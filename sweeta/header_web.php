6143bdb2c895183aebf7947bc0011ca9da532591
Ryouta021 <72176529+Ryouta021@users.noreply.github.com>
2020-12-22 22:59:28 +0700

    Add files via upload

---
+++ sweeta/header_web.php
@@ -0,0 +1,100 @@
+
+ <!--Header section start-->
+ <header class="header header-transparent header-sticky  d-lg-block d-none">
+            <div class="header-deafult-area">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2 col-md-4 col-12">
+                            <!--Logo Area Start-->
+                            <div class="logo-area">
+                                <a href="index.php"><img src="./assets/images/logo.png" alt=""></a>
+                                
+                            </div>
+                            <!--Logo Area End-->
+                            
+                        </div>
+                        <div class="col-xl-6 col-lg-6 col-md-4 d-none d-lg-block col-12">
+                            <!--Header Menu Area Start-->
+                            <div class="header-menu-area text-center">
+                                <nav class="main-menu">
+                                   
+                                    <ul>
+                                
+                                        <li><a href="shop.php">Sewa Baju</a>
+                                        </li>
+                                        
+                                        <li><a href="about.php">Tentang Kami</a>
+                                         
+                                        </li>
+                                        
+                                        <li><a class="header-search-toggle" href="#">Search</a></li>
+                                        
+                                        
+                                    </ul>
+                                </nav>
+                            </div>
+                            <!--Header Menu Area End-->
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-5 col-12">
+                            <!--Header Search And Mini Cart Area Start-->
+                            <div class="header-search-cart-area">
+                                <ul>
+                                    
+                                    
+                                    <li class="mini-cart"><a href="#"><i class="flaticon-shopping-cart"></i> <span class="mini-cart-total">Rp. 0</span></a>
+                                        <!--Mini Cart Dropdown Start-->
+                                        <div class="header-cart">
+                                            
+                                            <div class="cart-btn">
+                                                <a href="cart.php">View Cart</a>
+                                                <a href="checkout.php">checkout</a>
+                                            </div>
+                                        </div>
+                                        <!--Mini Cart Dropdown End-->
+                                    </li>
+                                    <li class="currency-menu"><a href="#"><i class="flaticon-user"></i></a>
+                                        <!--Crunccy dropdown-->
+                                        <ul class="currency-dropdown">
+                                    
+                                            <li><a>My account</a>
+                                                <ul>
+                                                    <li><a href="login.php">Login</a></li>
+                                                    <li><a href="logout.php">Logout</a></li>
+                                                    
+                                                </ul>
+                                            </li>
+                                            <!--Account Currency End-->
+                                        </ul>
+                                        <!--Crunccy dropdown-->
+                                    </li>
+                                    
+                                    
+                                </ul>
+                                
+                            </div>
+                            <!--Header Search And Mini Cart Area End-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!--Header section end-->
+<!-- main-search start -->
+<div class="main-search-active">
+            <div class="sidebar-search-icon">
+                <button class="search-close"><i class="fa fa-times"></i></button>
+            </div>
+            <div class="sidebar-search-input">
+                <form action="/search/" method="GET">
+                    <div class="form-search">
+                        <input id="search" name="keyword" class="input-text" value="" placeholder="" type="search">
+                        <button>
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+                <p class="form-description">Cari Barang Anda</p>
+            </div>
+        </div>
+        <!-- main-search start -->
+     
