9cdecf2abf744e86684a8fbcac79ef52e832fe03
Nadeaajeng <72237868+Nadeaajeng@users.noreply.github.com>
2020-12-23 09:18:43 +0700

    Add files via upload

---
+++ sweeta/login.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Sweeta - Sewa</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="assets/images/favicon.ico" type="img/x-icon" rel="shortcut icon">
+    <!-- All css files are included here. -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/helper.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Modernizr JS -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <?php 
+    require 'connect.php';
+    $sql = 'SELECT * FROM barang';
+    $result = mysqli_query($con, $sql);
+
+	
+	?>
+    <div id="main-wrapper">
+
+        <?php include "header_web.php" ?>
+
+        <!-- Page Banner Section Start -->
+        <div class="page-banner-section section bg-image" data-bg="assets/images/bg/breadcrumb.png">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+
+                        <div class="page-banner text-left">
+                            <h2>Login</h2>
+                            <ul class="page-breadcrumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li>Login</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Banner Section End -->
+        <!--Login Register section start-->
+        <div
+            class="login-register-section section pt-90 pt-lg-70 pt-md-60 pt-sm-55 pt-xs-45  pb-70 pb-lg-50 pb-md-40 pb-sm-30 pb-xs-20">
+            <div class="container">
+                <div class="row">
+                    <!--Login Form Start-->
+                    <div class="col-md-6 col-sm-6">
+                        <div class="customer-login-register">
+
+                            <div class="form-login-title">
+                                <h2>Login</h2>
+                            </div>
+                            <div class="login-form">
+                                <form action="cek_login" method="POST">
+                                    <div class="form-fild">
+                                        <p><label>Username or email address <span class="required">*</span></label></p>
+                                        <input name="username" value="" type="text">
+                                    </div>
+                                    <div class="form-fild">
+                                        <p><label>Password <span class="required">*</span></label></p>
+                                        <input name="password" value="" type="password">
+                                    </div>
+                                    <div class="login-submit">
+                                        <button type="submit" class="btn" value="login">Login</button>
+                                        <label>
+                                            <input class="checkbox" name="rememberme" value="" type="checkbox">
+                                            <span>Remember me</span>
+                                        </label>
+                                    </div>
+                                    <div class="lost-password">
+                                        <a href="register.php">Create Account?</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Login Form End--><?php if(isset($_GET['pesan'])){
+                        if($_GET['pesan']=="gagal"){
+                            echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+                        }
+                    }
+                    ?>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <!--Login Register section end-->
+
+        <!--Footer section start-->
+        <?php include "footer.php" ?>
+
+    </div>
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!-- All jquery file included here -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script
+        src="https://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.22&key=AIzaSyDAq7MrCR1A2qIShmjbtLHSKjcEIEBEEwM">
+    </script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
