c1bc26bf22763813024b7852cbdd360b6fc612f1
NaufalMahdi <71993847+NaufalMahdi@users.noreply.github.com>
2020-12-23 07:56:38 +0700

    Add files via upload

---
+++ sweeta/about.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Sweeta - Tentang Kami</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="assets/images/favicon.ico" type="img/x-icon" rel="shortcut icon">
+    <!-- All css files are included here. -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/helper.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Modernizr JS -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+
+    <div id="main-wrapper">
+
+        <!--Header section start-->
+        <?php include "header.php" ?>
+
+
+        <!-- Page Banner Section Start -->
+        <div class="page-banner-section section bg-image" data-bg="assets/images/bg/breadcrumb.png">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+
+                        <div class="page-banner text-left">
+                            <h2>Tentang Kami</h2>
+                            <ul class="page-breadcrumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li>Tentang Kami</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Banner Section End -->
+        <!--About Us Area Start-->
+        <div class="about-us-area section pt-90 pt-lg-70 pt-md-60 pt-sm-50 pt-xs-45 pb-70 pb-lg-50 pb-md-60 pb-sm-60 pb-xs-50">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-12">
+                        <!--About Us Image Start-->
+                        <div class="about-us-img-wrapper mb-30 mb-xs-15">
+                            <div class="about-us-image img-full">
+                                <a href="#"><img src="./assets/images/blog/blog-6.jpg" alt=""></a>
+                            </div>
+                        </div>
+                        <!--About Us Image End-->
+                    </div>
+                    <div class="col-lg-6 col-12">
+                        <!--About Us Content Start-->
+                        <div class="about-us-content pl-30 pl-md-0 pl-sm-0 pl-xs-0">
+                            <span>Sejak 1999</span>
+                            <h2>Sweeta Collection yang awalnya hanya melayani tata rias pengantin. </h2>
+                            <p>Sweeta Collection adalah usaha yang berkecimpung di bidang tata rias, wisuda, tari, karnaval dan tempat persewaan kostum.Dengan berjalannya waktu usaha yang di rintis oleh Triswita Nuswantarie ini kemudian lebih memfokuskan di bidang , tata rias dan persewaan kostum tari dan karnaval karena background nya memang seorang penari di masa sekolah dan kuliahnya sehingga pada saat pelaku seni tersebut menjadi seorang guru di salah satu SMP di kota Jember maka dia semakin menggeluti dunia seni tari dengan mengikuti berbagai kegiatan diklat dan pelatihan baik di kabupaten ataupun di propinsi.</p>
+                            <p> Melihat peluang bahwa ternyata di kabupaten Jember masih kurang sekali usaha di bidang tersebut maka Sweeta Collection semakin melebarkan sayap dengan mencoba membuat sendiri beragam kostum yg di kreasikan dan di sesuaikan dengan perkembangan jaman yang tentu saja tetap berpegang teguh pada budaya dan tradisi seni Indonesia khususnya untuk kepentingan tari kreasi, tari daerah dan kontemporer.</p>
+                        </div>
+                        <!--About Us Content End-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--About Us Area End-->
+
+
+
+
+         <!--Features section start-->
+        
+         <div class="features-section section pt-30 pt-lg-15 pt-md-0 pt-sm-0 pt-xs-15">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-6">
+                        <!-- Single Feature Start -->
+                        <div class="single-feature mb-30">
+                            
+                            <div class="feature-image">
+                                <img src="./assets/images/icons/feature-1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Gratis Ongkir</h4>
+                                <p class="short-desc">Area Jember</p>
+                            </div>
+                        </div>
+                        <!-- Single Feature End -->
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <!-- Single Feature Start -->
+                        <div class="single-feature mb-30">
+                            <div class="feature-image">
+                                <img src="./assets/images/icons/feature-2.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Kualitas Produk</h4>
+                                <p class="short-desc">Kualitas dan bahan terbaik</p>
+                            </div>
+                        </div>
+                        <!-- Single Feature End -->
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <!-- Single Feature Start -->
+                        <div class="single-feature mb-30">
+                            <div class="feature-image">
+                                <img src="./assets/images/icons/feature-3.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Diskon</h4>
+                                <p class="short-desc">Sewa Baju Termurah</p>
+                            </div>
+                        </div>
+                        <!-- Single Feature End -->
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+        <!--Features section end-->
+
+
+
+       
+
+
+     
+          <!--Footer section start-->
+          <?php include "footer.php" ?>
+        
+    </div>
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!-- All jquery file included here -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.22&key=AIzaSyDAq7MrCR1A2qIShmjbtLHSKjcEIEBEEwM"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ sweeta/privacy.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>nelson - Furniture eCommerce Bootstrap 4 Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link href="assets/images/favicon.ico" type="img/x-icon" rel="shortcut icon">
+    <!-- All css files are included here. -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/iconfont.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/helper.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Modernizr JS -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+
+    <div id="main-wrapper">
+
+        <!--Header section start-->
+        <?php include "header.php" ?>
+        <!--Header section end-->
+
+        
+        <!--Product Description Review Section Start-->
+        <div class="product-description-review-section section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="product-review-tab section">
+                            <!--Review And Description Tab Menu Start-->
+                            <ul class="nav dec-and-review-menu">
+                                <li>
+                                    <a class="active" data-toggle="tab" href="#description">Kebijakan Privasi</a>
+                                </li>
+                                <li>
+                                    <a data-toggle="tab" href="#reviews">Kebijakan Sweeta</a>
+                                </li>
+                            </ul>
+                            <!--Review And Description Tab Menu End-->
+                            <!--Review And Description Tab Content Start-->
+                            <div class="tab-content product-review-content-tab" id="myTabContent-4">
+                                <div class="tab-pane fade active show" id="description">
+                                    <div class="single-product-description">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero vulputate rutrum. Morbi ornare lectus quis justo gravida semper. Nulla tellus mi, vulputate adipiscing cursus eu, suscipit id nulla.</p>
+                                        <p>Pellentesque aliquet, sem eget laoreet ultrices, ipsum metus feugiat sem, quis fermentum turpis eros eget velit. Donec ac tempus ante. Fusce ultricies massa massa. Fusce aliquam, purus eget sagittis vulputate, sapien libero hendrerit est, sed commodo augue nisi non neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi. Cras neque metus, consequat et blandit et, luctus a nunc. Etiam gravida vehicula tellus, in imperdiet ligula euismod eget.</p>
+                                    </div>
+                                </div>
+                                <div class="tab-pane fade" id="reviews">
+                                    <div class="review-page-comment">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero vulputate rutrum. Morbi ornare lectus quis justo gravida semper. Nulla tellus mi, vulputate adipiscing cursus eu, suscipit id nulla.</p>
+                                        <p>Pellentesque aliquet, sem eget laoreet ultrices, ipsum metus feugiat sem, quis fermentum turpis eros eget velit. Donec ac tempus ante. Fusce ultricies massa massa. Fusce aliquam, purus eget sagittis vulputate, sapien libero hendrerit est, sed commodo augue nisi non neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem et placerat vestibulum, metus nisi posuere nisl, in accumsan elit odio quis mi. Cras neque metus, consequat et blandit et, luctus a nunc. Etiam gravida vehicula tellus, in imperdiet ligula euismod eget.</p>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Review And Description Tab Content End-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Product Description Review Section Start-->
+
+    
+       
+
+
+       
+
+         <!--Footer section start-->
+         <?php include "footer.php" ?>
+        <!--Footer section end-->
+        
+    </div>
+
+    <!-- Placed js at the end of the document so the pages load faster -->
+
+    <!-- All jquery file included here -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false&libraries=geometry&v=3.22&key=AIzaSyDAq7MrCR1A2qIShmjbtLHSKjcEIEBEEwM"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <script src="assets/js/plugins/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
