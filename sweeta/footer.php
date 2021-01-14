0522c627292f19ea0bb939dbdbbf014e356ec354
wildanhakim <muhwildanh@gmail.com>
2021-01-14 15:33:58 +0700

    update

--- sweeta/footer.php
+++ sweeta/footer.php
@@ -1,65 +1,79 @@
    <!--Footer section start-->
    <footer class="footer-section section bg-gray">
 
-<!--Footer Top start-->
-<div class="footer-top section pt-100 pt-lg-80 pt-md-70 pt-sm-60 pt-xs-50 pb-55 pb-lg-35 pb-md-25 pb-sm-15 pb-xs-10">
-    <div class="container">
-        <div class="row">
+       <!--Footer Top start-->
+       <div
+           class="footer-top section pt-100 pt-lg-80 pt-md-70 pt-sm-60 pt-xs-50 pb-55 pb-lg-35 pb-md-25 pb-sm-15 pb-xs-10">
+           <div class="container">
+               <div class="row">
 
-            <!--Footer Widget start-->
-            <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
-                <h4 class="title"><span class="text">Information</span></h4>
-                <ul class="ft-menu">
-                    <li><a href="privacy.php">Privacy Policy</a></li>
-                    <li><a href="about.php">About Us</a></li>
-                </ul>
-            </div>
-            <!--Footer Widget end-->
-            
-            <!--Footer Widget start-->
-            <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
-                <h4 class="title"><span class="text">Buka Setiap Hari nih</span></h4>
-                <p class="mb-15">Jam : 07.00 - 21.00</p>
-            </div>
-            <!--Footer Widget end-->
+                   <!--Footer Widget start-->
+                   <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
+                       <h4 class="title"><span class="text">Informasi</span></h4>
+                       <ul class="ft-menu">
+                           <li><a href="privacy.php">Kebijakan Privasi</a></li>
+                           <li><a href="about.php">Tentang Kami</a></li>
+                       </ul>
+                   </div>
+                   <!--Footer Widget end-->
 
-            <!--Footer Widget start-->
-            <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
-                
-            </div>
-            <!--Footer Widget end-->
+                   <!--Footer Widget start-->
+                   <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
+                       <h4 class="title"><span class="text">Buka Setiap Hari nih</span></h4>
+                       <p class="mb-15">Jam : 07.00 - 21.00</p>
+                   </div>
+                   <!--Footer Widget end-->
 
-            <!--Footer Widget start-->
-            <div class="footer-widget text-lg-right text-left col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
-                <h2>SWEETA</h2>
-                <p class="contact-text">Jl.Argopuro 3 C-4</p>
-                <p class="contact-text">Jember, Jawa Timur</p>
-                <p class="contact-text">sweeta.rental@gmail.com</p>
-                <p class="contact-text">+62 838 3574 2425</p>
-            </div>
-            <!--Footer Widget end-->
-        </div>
-    </div>
-</div>
-<!--Footer Top end-->
+                   <!--Footer Widget start-->
+                   <div class="footer-widget col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
+                       <div class="header-search-cart-area">
+                           <ul>
+                               <li class="currency-menu"><a href="login.php"><i class="flaticon-user"></i></a>
+                                   <!--Crunccy dropdown-->
+                                   <ul class="currency-dropdown">
+                                       <li><a href="login.php">Login</a></li>      
+                                       <!--Account Currency End-->
+                                   </ul>
+                                   <!--Crunccy dropdown-->
+                               </li>
+                           </ul>
+                       </div>
+                   </div>
+                   <!--Footer Widget end-->
 
-<!--Footer bottom start-->
-<div class="footer-bottom section">
-    <div class="container">
-        <div class="row no-gutters">
-            <div class="col-12 ft-border pt-25 pb-25">
-                <div class="row justify-content-between align-items-center">
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                        <div class="copyright text-left">
-                            <p>Copyright &copy;2020 <a href="#">sweeta</a>. All rights reserved.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!--Footer bottom end-->
+                   <!--Footer Widget start-->
+                   <div
+                       class="footer-widget text-lg-right text-left col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-40 mb-xs-35">
+                       <h2>SWEETA</h2>
+                       <p class="contact-text">Jl.Argopuro 3 C-4</p>
+                       <p class="contact-text">Jember, Jawa Timur</p>
+                       <p class="contact-text">sweeta.rental@gmail.com</p>
+                       <p class="contact-text">+62 838 3574 2425</p>
+                   </div>
+                   <!--Footer Widget end-->
+               </div>
+           </div>
+       </div>
+       <!--Footer Top end-->
 
-</footer>
-<!--Footer section end-->
+       <!--Footer bottom start-->
+       <div class="footer-bottom section">
+           <div class="container">
+               <div class="row no-gutters">
+                   <div class="col-12 ft-border pt-25 pb-25">
+                       <div class="row justify-content-between align-items-center">
+                           <div class="col-lg-6 col-md-6 col-sm-12">
+                               <div class="copyright text-left">
+                                   <p>Copyright &copy;2020 <a href="#">sweeta</a>. All rights reserved.</p>
+
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <!--Footer bottom end-->
+
+   </footer>
+   <!--Footer section end-->
